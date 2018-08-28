425b7660d572bccba9b5184858d2559c9a6ae05f
GalvinGao <GalvinGao@users.noreply.github.com>
2018-08-28 20:28:13 +0800

    Initial commit

---
+++ src/intro/chapter3/Float.java
@@ -0,0 +1,11 @@
+package intro.chapter3;
+
+public class Float {
+    public static void main(String[] args) {
+        char x='哈';
+        char y='哈';
+        char z='哈';
+        char a='哈';
+        System.out.println("Hey: " + x + y + z + a);
+    }
+}
---
+++ src/intro/chapter3/Int.java
@@ -0,0 +1,8 @@
+package intro.chapter3;
+
+public class Int {
+    public static void main(String[] args) {
+        int a = 1;
+        System.out.println(a);
+    }
+}
